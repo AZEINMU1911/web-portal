39d592aa704a4b79c8441dc089ff572225356639
Alfikri Z.M <alfzein19@gmail.com>
2024-05-28 19:31:42 +0300

    Added initial setup and testing if everything runs ok

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 const express = require ('express')
+const bodyParser = require ('body-parser')
+
+const app = express()
+
+app.use = (express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const port = process.env.PORT || 3000
+
+//Backend stuff
+app.get ('/',(req,res) => {
+    res.send("Hello World!")
+})
+//Backend stuff
+
+app.listen (port, () =>[
+    console.log `Port opened at ${port}`
+])
